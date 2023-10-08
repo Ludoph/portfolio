f0e77709bde033ee817126e459775aee12c73c35
ludoph <phounphonh.ludovic@gmail.com>
2023-10-08 21:02:22 +0200

    feat: add project page and link about to project

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,11 +2,15 @@ import React from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function About() {
   useEffect(() => {
     Aos.init({ duration: 3000 });
   }, []);
+
+
+
   return (
     <div className="about">
       <h1
@@ -17,18 +21,9 @@ function About() {
       >
         about me
       </h1>
-      <div
-        className="about-me"
-        
-      >
-        <h3
-          data-aos="fade-down"
-        >
-          Hi, I'm Fullstack Developer
-        </h3>
-        <p
-          data-aos="fade-up"
-        >
+      <div className="about-me">
+        <h3 data-aos="fade-down">Hi, I'm Fullstack Developer</h3>
+        <p data-aos="fade-up">
           A passionate developer and apprentice who is enthusiastic about
           learning and practicing new things. Your curiosity drives you to
           explore and discover new ideas, and you are always looking for ways to
@@ -36,6 +31,7 @@ function About() {
           your knowledge to contribute to the development of innovative
           projects.
         </p>
+        <Link to="/project">My Work</Link>
       </div>
     </div>
   );
---
+++ src/pages/Project.js
@@ -0,0 +1,35 @@
+import React from "react";
+import img1 from "../assets/img02.png";
+import img2 from "../assets/img04.png";
+import img3 from "../assets/img05.png";
+import img4 from "../assets/img03.jpg";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
+export default function Project() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <div className="project" data-aos="fade-in" data-aos-easing="ease-in-sine">
+      <div className="project-items">
+        <p># D.Hub</p>
+        <img src={img1} alt="d.hub" />
+      </div>
+      <div className="project-items">
+        <p># OLA BEL BUL</p>
+        <img src={img2} alt="d.hub" />
+      </div>
+      <div className="project-items">
+        <p># POKEDEX</p>
+        <img src={img3} alt="d.hub" />
+      </div>
+      <div className="project-items">
+        <p># RESTO LAO</p>
+        <img src={img4} alt="d.hub" style={{ objectFit: "contain" }} />
+      </div>
+    </div>
+  );
+}
